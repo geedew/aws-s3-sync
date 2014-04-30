9a661334e4413a8b27691fe2e29a188b0dcab660
Drew Wilson <drew@geedew.com>
2014-04-30 07:13:47 -0500

    Adding notes

--- src/lib/s3sync.js
+++ src/lib/s3sync.js
@@ -10,4 +10,22 @@ AWS.config.loadFromPath(APPPATH + '/config.json');
 var s3 = new AWS.S3();
 var s3sync = {};
 
+s3sync.sync = function(options, callback) {
+  var response;
+  var params = { };
+
+ // loop through files
+   // Make sure bucket exists if set
+     // Create bucket if not existent
+   // Set Params
+     // encoding, md5, body, ACL (if set), expires, encryption, 
+   // Put Object, save Callback for a 'when' statement
+
+  if(typeof callback !== undefined) {
+  	return callback(response);
+  } else {
+ 	return response;
+  }
+};
+
 module.exports = s3sync;
