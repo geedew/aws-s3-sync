0d3456ce7d3c79ac74804293f52fb70338799381
Drew Wilson <drew@geedew.com>
2014-05-01 08:34:30 -0500

    Adding Q; checking for json file

--- src/lib/s3sync.js
+++ src/lib/s3sync.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var Q = require('q');
 var APPPATH = process.cwd();
 var fs = require('fs');
 
@@ -6,14 +7,20 @@ if(!process.env.AMAZON_ACCESS_KEY_ID && !process.env.AWS_ACCESS_KEY_ID && !fs.ex
   throw new Error('You must create a `config.json` or set environment variables to use this library'); 
 }
 
-AWS.config.loadFromPath(APPPATH + '/config.json');
+if(fs.exsistsSync(APPPATH + '/config.json')) {
+  AWS.config.loadFromPath(APPPATH + '/config.json');
+}
+
 var s3 = new AWS.S3();
 var s3sync = {};
 
-s3sync.sync = function(options, callback) {
+s3sync.sync = function(options) {
   var response;
   var params = { };
 
+  return function() {
+  };
+  
  // loop through files
    // Make sure bucket exists if set
      // Create bucket if not existent
@@ -21,11 +28,6 @@ s3sync.sync = function(options, callback) {
      // encoding, md5, body, ACL (if set), expires, encryption, 
    // Put Object, save Callback for a 'when' statement
 
-  if(typeof callback !== undefined) {
-  	return callback(response);
-  } else {
- 	return response;
-  }
 };
 
 module.exports = s3sync;
